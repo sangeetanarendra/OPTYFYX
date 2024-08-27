e92c2bfe67548b4518469e5f37dd241d289c3ac5
Sangeeta Narendra <165122313+sangeetanarendra@users.noreply.github.com>
2024-08-27 01:40:30 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.navbar .nav-links a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
